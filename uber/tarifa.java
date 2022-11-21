f6af93dbce55c63d6e30d6338dc4c4b064b7c91a
AlekiBr <102824980+AlekiBr@users.noreply.github.com>
2022-11-21 15:12:54 -0300

    Delete uber directory

--- uber/tarifa.java
+++
@@ -1,9 +0,0 @@
-package uber;
-interface uber{                                      //implementamos a interface, logo mais com os exceptions
-
-    public class tarifa extends viagem{
-
-}
-    void mostrarvalor();
-
-}
